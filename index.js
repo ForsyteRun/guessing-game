96625afe8e23a2f6b8aa3acb4a5d444dc519b4a4
ForsyteRun <forsyterun@gmail.com>
2023-02-26 06:42:49 +0200

    feat: add solution

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const GuessingGame = require('./src/guessing-game.js');
-
 window.game = new GuessingGame();
+
+// class GuessingGame extends GuessingGame{
+//    constructor(res, min, max){
+//       res,
+//       min, 
+//       max
+//    }
+
+//    setRange(min, max){
+//       this.min = min
+//       this.max = max
+//    }
+
+//    guess(){
+//       let arr = []
+//       for (let index = this.min; index <= this.max; index++) {
+//          arr.push(index)
+//       }
+//       this.res = arr[Math.round((arr.length - 1) / 2)]
+//       return this.res
+//    }
+
+//    lower(){
+//      this.max = this.res
+//    }
+
+//    greater(){
+//      this.min = this.res
+//    }
+// }
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor(res, min, max) {
+      res,
+      min, 
+      max
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        let arr = []
+        for (let index = this.min; index <= this.max; index++) {
+           arr.push(index)
+        }
+        this.res = arr[Math.round((arr.length - 1) / 2)]
+        return this.res
     }
 
     lower() {
-
+     this.max = this.res
     }
 
     greater() {
+     this.min = this.res
 
     }
 }
